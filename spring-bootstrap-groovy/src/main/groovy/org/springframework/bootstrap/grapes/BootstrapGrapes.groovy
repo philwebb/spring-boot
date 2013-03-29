dcc7ea509f177cd504c39ada3f97e4fccdfe7c1a
Phillip Webb <pwebb@vmware.com>
2013-03-28 18:30:15 -0700

    Comment @EnableAutoConfiguration due to error
    [INFO] --- exec-maven-plugin:1.2.1:exec (compile-plugin) @ spring-bootstrap-groovy ---
    >>> a serious error occurred: BUG! exception in phase 'semantic analysis' in source unit 'src/main/groovy/org/springframework/bootstrap/grapes/BootstrapGrapes.groovy' sun.reflect.annotation.TypeNotPresentExceptionProxy
    >>> stacktrace:
    BUG! exception in phase 'semantic analysis' in source unit 'src/main/groovy/org/springframework/bootstrap/grapes/BootstrapGrapes.groovy' sun.reflect.annotation.TypeNotPresentExceptionProxy
    	at org.codehaus.groovy.control.CompilationUnit.applyToSourceUnits(CompilationUnit.java:916)
    	at org.codehaus.groovy.control.CompilationUnit.doPhaseOperation(CompilationUnit.java:575)
    	at org.codehaus.groovy.control.CompilationUnit.compile(CompilationUnit.java:524)
    	at org.codehaus.groovy.control.CompilationUnit.compile(CompilationUnit.java:507)
    	at org.codehaus.groovy.tools.FileSystemCompiler.compile(FileSystemCompiler.java:59)
    	at org.codehaus.groovy.tools.FileSystemCompiler.doCompilation(FileSystemCompiler.java:215)
    	at org.codehaus.groovy.tools.FileSystemCompiler.commandLineCompile(FileSystemCompiler.java:148)
    	at org.codehaus.groovy.tools.FileSystemCompiler.commandLineCompileWithErrorHandling(FileSystemCompiler.java:178)
    	at org.codehaus.groovy.tools.FileSystemCompiler.main(FileSystemCompiler.java:162)
    	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
    	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:57)
    	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
    	at java.lang.reflect.Method.invoke(Method.java:601)
    	at org.codehaus.groovy.tools.GroovyStarter.rootLoader(GroovyStarter.java:106)
    	at org.codehaus.groovy.tools.GroovyStarter.main(GroovyStarter.java:128)
    Caused by: java.lang.ArrayStoreException: sun.reflect.annotation.TypeNotPresentExceptionProxy
    	at sun.reflect.annotation.AnnotationParser.parseClassArray(AnnotationParser.java:673)
    	at sun.reflect.annotation.AnnotationParser.parseArray(AnnotationParser.java:480)
    	at sun.reflect.annotation.AnnotationParser.parseMemberValue(AnnotationParser.java:306)
    	at sun.reflect.annotation.AnnotationParser.parseAnnotation(AnnotationParser.java:241)
    	at sun.reflect.annotation.AnnotationParser.parseAnnotations2(AnnotationParser.java:88)
    	at sun.reflect.annotation.AnnotationParser.parseAnnotations(AnnotationParser.java:70)
    	at java.lang.Class.initAnnotationsIfNecessary(Class.java:3089)
    	at java.lang.Class.getAnnotation(Class.java:3048)
    	at org.codehaus.groovy.control.ResolveVisitor.visitAnnotations(ResolveVisitor.java:1051)
    	at org.codehaus.groovy.ast.ClassCodeVisitorSupport.visitClass(ClassCodeVisitorSupport.java:47)
    	at org.codehaus.groovy.control.ResolveVisitor.visitClass(ResolveVisitor.java:1176)
    	at org.codehaus.groovy.control.ResolveVisitor.startResolving(ResolveVisitor.java:141)
    	at org.codehaus.groovy.control.CompilationUnit$10.call(CompilationUnit.java:633)
    	at org.codehaus.groovy.control.CompilationUnit.applyToSourceUnits(CompilationUnit.java:912)
    	... 14 more

--- spring-bootstrap-groovy/src/main/groovy/org/springframework/bootstrap/grapes/BootstrapGrapes.groovy
+++ spring-bootstrap-groovy/src/main/groovy/org/springframework/bootstrap/grapes/BootstrapGrapes.groovy
@@ -3,8 +3,8 @@ package org.springframework.bootstrap.grapes
 // Spring stuff needs to be on the system classloader apparently (when using @Configuration)
 @GrabResolver(name='spring-snapshot', root='http://repo.springframework.org/snapshot')
 @GrabConfig(systemClassLoader=true)
-@Grab("org.springframework.bootstrap:spring-bootstrap:0.0.1-SNAPSHOT")
 @Grab("org.springframework:spring-context:4.0.0.BOOTSTRAP-SNAPSHOT")
+@Grab("org.springframework.bootstrap:spring-bootstrap:0.0.1-SNAPSHOT")
 @GrabExclude("commons-logging:commons-logging")
 @Grab("org.slf4j:jcl-over-slf4j:1.6.1")
 @Grab("org.slf4j:slf4j-jdk14:1.6.1")
@@ -13,7 +13,8 @@ class BootstrapGrapes {
 
 import org.springframework.bootstrap.context.annotation.EnableAutoConfiguration
 import org.springframework.context.annotation.Configuration
-@EnableAutoConfiguration
+
 @Configuration
+// @EnableAutoConfiguration
 class BootstrapAutoConfiguration { 
 }
