ea3104bd3690b3101f00da3290ab27ff59820cd1
Phillip Webb <pwebb@gopivotal.com>
2013-08-30 00:40:56 -0700

    Hacking

--- prepare-ci-build.sh
+++ prepare-ci-build.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
-rm build-release-version.*
-touch build-release-version.milestone
+
+# This script is executed by the CI server before each build
+
+# Write a file that we can use to set Maven Profiles
+rm building-version.* 2>/dev/null
+version=`cat pom.xml| grep -m 1 "<version>.*</version>"`
+echo "$version" | grep -q "\-SNAPSHOT<"
+if [ $? -eq 0 ];then
+	echo "Setting 'building.version' profile file to SNAPSHOT"
+	touch building-version.snapshot
+fi
+echo "$version" | grep -q "\.M.*<"
+if [ $? -eq 0 ];then
+	echo "Setting 'building.version' profile file to MILESTONE"
+	touch building-version.milestone
+fi
+echo "$version" | grep -q "\.RELEASE<"
+if [ $? -eq 0 ];then
+	echo "Setting 'building.version' profile file to RELEASE"
+	touch building-version.release
+fi
+
+
+# Clean Grapes
+if [ -z "${GROOVY_HOME}" ];then
+  GROOVY_HOME=~/.groovy
+fi
+echo "Cleaning ${GROOVY_HOME}/grapes"
+rm -rf "${GROOVY_HOME}"/grapes 2>/dev/null
