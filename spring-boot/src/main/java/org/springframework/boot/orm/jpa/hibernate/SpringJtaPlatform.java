1fd5be543e2d1aa06a6a54c2d02ad7dd13cd9cd2
Josh Long <jlong@pivotal.io>
2014-08-22 11:55:51 -0700

    Add SpringJtaPlatform for Hibernate
    Add SpringJtaPlatform Hibernate AbstractJtaPlatform implementation. Also
    relocated SpringNamingStrategy to the `hibernate` sub-package.
    
    See gh-947

---
+++ spring-boot/src/main/java/org/springframework/boot/orm/jpa/hibernate/SpringJtaPlatform.java
@@ -0,0 +1,44 @@
+package org.springframework.boot.orm.jpa.hibernate;
+
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
+
+import org.hibernate.engine.transaction.jta.platform.internal.AbstractJtaPlatform;
+import org.springframework.transaction.jta.JtaTransactionManager;
+import org.springframework.util.Assert;
+
+/**
+ * Generic Hibernate {@link AbstractJtaPlatform} implementation that simply resolves the
+ * JTA {@link UserTransaction} and {@link TransactionManager} from the Spring-configured
+ * {@link JtaTransactionManager} implementation.
+ *
+ * @author Josh Long
+ * @author Phillip Webb
+ * @since 1.2.0
+ */
+public class SpringJtaPlatform extends AbstractJtaPlatform {
+
+	private static final long serialVersionUID = 1L;
+
+	private final JtaTransactionManager transactionManager;
+
+	public SpringJtaPlatform(JtaTransactionManager transactionManager) {
+		Assert.notNull(transactionManager, "TransactionManager must not be null");
+		this.transactionManager = transactionManager;
+	}
+
+	protected boolean hasTransactionManager() {
+		return true;
+	}
+
+	@Override
+	protected TransactionManager locateTransactionManager() {
+		return this.transactionManager.getTransactionManager();
+	}
+
+	@Override
+	protected UserTransaction locateUserTransaction() {
+		return this.transactionManager.getUserTransaction();
+	}
+
+}
