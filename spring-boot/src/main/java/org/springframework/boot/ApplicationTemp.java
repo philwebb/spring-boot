0c394d0fbcfe4943c775724d51c28f2d5f1ad515
Phillip Webb <pwebb@pivotal.io>
2015-07-13 16:36:19 -0700

    Add tempfolder

---
+++ spring-boot/src/main/java/org/springframework/boot/ApplicationTemp.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.util.Random;
+
+/**
+ * Provides access to an application specific temporary folder. Generally speaking
+ * different Spring Boot applications will get different locations, however, simply
+ * restarting an application will give the same location.
+ *
+ * @author Phillip Webb
+ * @since 1.3.0
+ */
+public class ApplicationTemp {
+
+	private static final char[] HEX_CHARS = "0123456789ABCDEF".toCharArray();
+
+	private final Class<?> sourceClass;
+
+	private volatile File folder;
+
+	/**
+	 * Create a new {@link ApplicationTemp} instance.
+	 */
+	public ApplicationTemp() {
+		this(null);
+	}
+
+	/**
+	 * Create a new {@link ApplicationTemp} instance for the specified source class.
+	 * @param sourceClass the source class or {@code null}
+	 */
+	public ApplicationTemp(Class<?> sourceClass) {
+		this.sourceClass = sourceClass;
+	}
+
+	@Override
+	public String toString() {
+		try {
+			return getFolder().getAbsolutePath();
+		}
+		catch (IOException ex) {
+			throw new IllegalStateException(ex);
+		}
+	}
+
+	public File getFolder() throws IOException {
+		if (this.folder == null) {
+			this.folder = calculateFolder();
+		}
+		return this.folder;
+	}
+
+	private File calculateFolder() throws IOException {
+		byte[] hash = generateHash(this.sourceClass);
+		File tempDirectory = getTempDirectory();
+		if (hash == null || tempDirectory == null || !tempDirectory.exists()) {
+			return getRealTempFolder();
+		}
+		return new File(tempDirectory, toHexString(hash));
+	}
+
+	private File getTempDirectory() {
+		String temp = System.getProperty("java.io.tmpdir");
+		return (temp == null ? null : new File(temp));
+	}
+
+	private File getRealTempFolder() throws IOException {
+		byte[] random = new byte[20];
+		new Random().nextBytes(random);
+		File temp = File.createTempFile(toHexString(random), null);
+		temp.delete();
+		temp.mkdir();
+		temp.deleteOnExit();
+		return temp;
+	}
+
+	private byte[] generateHash(Class<?> sourceClass) {
+		ApplicationHome home = new ApplicationHome(sourceClass);
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("SHA-1");
+			update(digest, home.getSource());
+			update(digest, home.getDir());
+			update(digest, System.getProperty("user.dir"));
+			update(digest, System.getProperty("java.home"));
+			update(digest, System.getProperty("java.class.path"));
+			update(digest, System.getProperty("sun.java.command"));
+			update(digest, System.getProperty("sun.boot.class.path"));
+			return digest.digest();
+		}
+		catch (Exception ex) {
+			return null;
+		}
+	}
+
+	private void update(MessageDigest digest, Object source) {
+		if (source != null) {
+			digest.update(getUpdateSourceBytes(source));
+		}
+	}
+
+	private byte[] getUpdateSourceBytes(Object source) {
+		if (source instanceof File) {
+			return getUpdateSourceBytes(((File) source).getAbsolutePath());
+		}
+		return source.toString().getBytes();
+	}
+
+	private String toHexString(byte[] bytes) {
+		char[] hex = new char[bytes.length * 2];
+		for (int i = 0; i < bytes.length; i++) {
+			int b = bytes[i] & 0xFF;
+			hex[i * 2] = HEX_CHARS[b >>> 4];
+			hex[i * 2 + 1] = HEX_CHARS[b & 0x0F];
+		}
+		return new String(hex);
+	}
+
+}
