a918766a33c2c1ff4f403690c34ce5508c10575c
Phillip Webb <pwebb@pivotal.io>
2016-03-11 23:19:48 -0800

    Unify boot strappers
    fixup

--- spring-boot/src/main/java/org/springframework/boot/context/web/ServletContextApplicationContextInitializer.java
+++ spring-boot/src/main/java/org/springframework/boot/context/web/ServletContextApplicationContextInitializer.java
@@ -18,14 +18,17 @@ package org.springframework.boot.context.web;
 
 import javax.servlet.ServletContext;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.core.Ordered;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * {@link ApplicationContextInitializer} for setting the servlet context.
  *
  * @author Dave Syer
+ * @author Phillip Webb
  */
 public class ServletContextApplicationContextInitializer implements
 		ApplicationContextInitializer<ConfigurableWebApplicationContext>, Ordered {
@@ -34,12 +37,27 @@ public class ServletContextApplicationContextInitializer implements
 
 	private final ServletContext servletContext;
 
+	private final boolean addApplicationContextAttribute;
+
 	/**
 	 * Create a new {@link ServletContextApplicationContextInitializer} instance.
 	 * @param servletContext the servlet that should be ultimately set.
 	 */
 	public ServletContextApplicationContextInitializer(ServletContext servletContext) {
+		this(servletContext, false);
+	}
+
+	/**
+	 * Create a new {@link ServletContextApplicationContextInitializer} instance.
+	 * @param servletContext the servlet that should be ultimately set.
+	 * @param addApplicationContextAttribute if the {@link ApplicationContext} should be
+	 * stored as an attribute in the {@link ServletContext}
+	 * @since 1.4.0
+	 */
+	public ServletContextApplicationContextInitializer(ServletContext servletContext,
+			boolean addApplicationContextAttribute) {
 		this.servletContext = servletContext;
+		this.addApplicationContextAttribute = addApplicationContextAttribute;
 	}
 
 	public void setOrder(int order) {
@@ -54,6 +72,12 @@ public class ServletContextApplicationContextInitializer implements
 	@Override
 	public void initialize(ConfigurableWebApplicationContext applicationContext) {
 		applicationContext.setServletContext(this.servletContext);
+		if (this.addApplicationContextAttribute) {
+			this.servletContext.setAttribute(
+					WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE,
+					applicationContext);
+		}
+
 	}
 
 }
