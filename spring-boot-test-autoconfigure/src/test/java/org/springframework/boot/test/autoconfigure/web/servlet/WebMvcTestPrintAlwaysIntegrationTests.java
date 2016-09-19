c4a8744f89123622b671852a7f72e71ba05962e8
Phillip Webb <pwebb@pivotal.io>
2016-09-18 17:23:21 -0700

    Polish

--- spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/web/servlet/WebMvcTestPrintAlwaysIntegrationTests.java
+++ spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/web/servlet/WebMvcTestPrintAlwaysIntegrationTests.java
@@ -16,13 +16,16 @@
 
 package org.springframework.boot.test.autoconfigure.web.servlet;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,22 +36,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Phillip Webb
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(secure = false)
-public class WebMvcTestPrintDefaultIntegrationTests {
+@WebMvcTest
+@AutoConfigureMockMvc(secure = false, printOnlyOnFailure = false)
+public class WebMvcTestPrintAlwaysIntegrationTests {
+
+	@Rule
+	public OutputCapture output = new OutputCapture();
 
 	@Autowired
 	private MockMvc mvc;
 
 	@Test
-	public void shouldNotPrint() throws Exception {
+	public void shouldPrint() throws Exception {
 		this.mvc.perform(get("/one")).andExpect(content().string("one"))
 				.andExpect(status().isOk());
-	}
-
-	@Test
-	public void shouldPrint() throws Exception {
-		this.mvc.perform(get("/one")).andExpect(content().string("notone"))
-				.andExpect(status().isOk());
+		assertThat(this.output.toString()).contains("Request URI = /one");
 	}
 
 }
