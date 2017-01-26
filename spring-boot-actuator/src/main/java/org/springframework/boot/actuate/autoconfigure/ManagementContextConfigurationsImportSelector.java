459b9192056b28f8f45e40c920582cbbee647ceb
Andy Wilkinson <awilkinson@pivotal.io>
2017-01-26 19:48:10 +0000

    Order ManagementContextConfiguration classes without loading them
    Previously, ManagementContextConfiguration classes were loaded to
    allow them to be ordered based on either @Order or implementing
    Ordered. This had the unwanted side-effect of possibly logging
    unwanted INFO messages if the reflection-based annotation
    introspection failed. One cause of this was @ConditionalOnClass when
    the referenced class was not on the classpath.
    
    This commit uses the ASM-based annotation metadata reading to
    determine the order of a management context configuration class based
    on the @Order annotation. The classes are then sorted using a standard
    OrderComparator. Note that Ordering via implemented Ordered is not
    supported as it cannot be determine without loading the class.

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/autoconfigure/ManagementContextConfigurationsImportSelector.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/autoconfigure/ManagementContextConfigurationsImportSelector.java
@@ -16,19 +16,20 @@
 
 package org.springframework.boot.actuate.autoconfigure;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.OrderComparator;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.support.SpringFactoriesLoader;
 import org.springframework.core.type.AnnotationMetadata;
-import org.springframework.util.ClassUtils;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.SimpleMetadataReaderFactory;
 
 /**
  * Selects configuration classes for the management context configuration. Entries are
@@ -38,6 +39,7 @@ import org.springframework.util.ClassUtils;
  *
  * @author Dave Syer
  * @author Phillip Webb
+ * @author Andy Wilkinson
  * @see ManagementContextConfiguration
  */
 @Order(Ordered.LOWEST_PRECEDENCE)
@@ -48,13 +50,43 @@ class ManagementContextConfigurationsImportSelector
 
 	@Override
 	public String[] selectImports(AnnotationMetadata metadata) {
-		// Find all possible auto configuration classes, filtering duplicates
+		// Find all possible management context configuration classes, filtering
+		// duplicates
 		List<String> names = loadFactoryNames();
-		Set<Class<?>> classes = new LinkedHashSet<Class<?>>();
-		for (String factoryName : names) {
-			classes.add(ClassUtils.resolveClassName(factoryName, this.classLoader));
+		SimpleMetadataReaderFactory readerFactory = new SimpleMetadataReaderFactory(
+				this.classLoader);
+		List<ManagementContextConfigurationClass> configurationClasses = createConfigurationClasses(
+				names, readerFactory);
+		OrderComparator.sort(configurationClasses);
+		List<String> sortedNames = new ArrayList<String>();
+		for (ManagementContextConfigurationClass configurationClass : configurationClasses) {
+			sortedNames.add(configurationClass.className);
 		}
-		return getSortedClassNames(new ArrayList<Class<?>>(classes));
+		return sortedNames.toArray(new String[names.size()]);
+	}
+
+	private List<ManagementContextConfigurationClass> createConfigurationClasses(
+			List<String> names, SimpleMetadataReaderFactory readerFactory) {
+		List<ManagementContextConfigurationClass> configurationClasses = new ArrayList<ManagementContextConfigurationClass>();
+		for (String name : names) {
+			try {
+				MetadataReader metadataReader = readerFactory.getMetadataReader(name);
+				Map<String, Object> orderAttributes = metadataReader
+						.getAnnotationMetadata()
+						.getAnnotationAttributes(Order.class.getName());
+				int order = (orderAttributes != null
+						&& orderAttributes.get("value") != null)
+								? (int) orderAttributes.get("value")
+								: Ordered.LOWEST_PRECEDENCE;
+				configurationClasses
+						.add(new ManagementContextConfigurationClass(name, order));
+			}
+			catch (IOException ex) {
+				throw new RuntimeException(
+						"Failed to read annotation metadata for '" + name + "'", ex);
+			}
+		}
+		return configurationClasses;
 	}
 
 	protected List<String> loadFactoryNames() {
@@ -62,18 +94,27 @@ class ManagementContextConfigurationsImportSelector
 				.loadFactoryNames(ManagementContextConfiguration.class, this.classLoader);
 	}
 
-	private String[] getSortedClassNames(List<Class<?>> classes) {
-		AnnotationAwareOrderComparator.sort(classes);
-		List<String> names = new ArrayList<String>();
-		for (Class<?> sourceClass : classes) {
-			names.add(sourceClass.getName());
-		}
-		return names.toArray(new String[names.size()]);
-	}
-
 	@Override
 	public void setBeanClassLoader(ClassLoader classLoader) {
 		this.classLoader = classLoader;
+	}
+
+	private static class ManagementContextConfigurationClass implements Ordered {
+
+		private final String className;
+
+		private final int order;
+
+		public ManagementContextConfigurationClass(String className, int order) {
+			this.className = className;
+			this.order = order;
+		}
+
+		@Override
+		public int getOrder() {
+			return this.order;
+		}
+
 	}
 
 }
