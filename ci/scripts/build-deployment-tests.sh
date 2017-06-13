62d279b7765290540922998132013c3d81d9c429
Phillip Webb <pwebb@pivotal.io>
2017-06-12 22:08:27 -0700

    Hacking ci

---
+++ ci/scripts/build-deployment-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+source $(dirname $0)/common.sh
+repository=$(pwd)/distribution-repository
+
+setup_symlinks
+cleanup_maven_repo
+
+pushd spring-boot > /dev/null
+run_maven -f spring-boot-deployment-tests/pom.xml clean install -U -Dfull -Drepository=file://${repository}
+popd > /dev/null
---
+++ ci/scripts/common.sh
@@ -0,0 +1,17 @@
+setup_symlinks() {
+	mkdir -p m2 && ln -s "$(pwd)/m2" ~/.m2
+	mkdir -p gradle && ln -s "$(pwd)/gradle" ~/.gradle
+}
+
+cleanup_maven_repo() {
+	rm -fr ~/.m2/repository/org/springframework/boot 2> /dev/null || :
+}
+
+run_maven() {
+	echo "./mvnw $@" > build.log
+	((tail -n 0 -q -f build.log & echo $! >&3) 3>pid | grep -v --color=never "\] \[INFO\]\|Building\ jar" | grep --color=never "\[INFO.*---\|Building\ \|SUCCESS\|Total\ time\|Finished\ at\|Final\ Memory" &)
+	echo "./mvnw $@"
+	./mvnw "$@" >> build.log 2>&1 || (kill $(<pid) && sleep 1 && tail -n 3000 build.log && exit 1)
+	kill $(<pid)
+	sleep 1
+}
