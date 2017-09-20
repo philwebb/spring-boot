26e3e01f3443ea8a6dc4e587ed6acb3855bfb4f5
Phillip Webb <pwebb@pivotal.io>
2017-09-19 18:01:22 -0700

    Hacking CI

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -4,9 +4,6 @@ set -e
 source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
-setup_symlinks
-cleanup_maven_repo
-
-pushd spring-boot > /dev/null
+pushd git-repo > /dev/null
 run_maven -f spring-boot-project/pom.xml clean deploy -U -Dfull -DaltDeploymentRepository=distribution::default::file://${repository}
 popd > /dev/null
--- ci/scripts/common.sh
+++ ci/scripts/common.sh
@@ -1,17 +1,4 @@
-setup_symlinks() {
-	mkdir -p m2 && ln -s "$(pwd)/m2" ~/.m2
-	mkdir -p gradle && ln -s "$(pwd)/gradle" ~/.gradle
-}
+source /opt/concourse-java.sh
 
-cleanup_maven_repo() {
-	rm -fr ~/.m2/repository/org/springframework/boot 2> /dev/null || :
-}
-
-run_maven() {
-	echo "./mvnw $@" > build.log
-	((tail -n 0 -q -f build.log & echo $! >&3) 3>pid | grep -v --color=never "\] \[INFO\]\|Building\ jar" | grep --color=never "\[INFO.*---\|Building\ \|SUCCESS\|Total\ time\|Finished\ at\|Final\ Memory" &)
-	echo "./mvnw $@"
-	./mvnw "$@" >> build.log 2>&1 || (kill $(<pid) && sleep 1 && tail -n 3000 build.log && exit 1)
-	kill $(<pid)
-	sleep 1
-}
+setup_symlinks
+cleanup_maven_repo "org.springframework.boot"
