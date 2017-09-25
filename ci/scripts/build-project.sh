b7747475ff8ccd1f3a6f858841f0dcabf66ea8e5
Phillip Webb <pwebb@pivotal.io>
2017-09-25 15:26:55 -0700

    Hacking

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,6 +5,5 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
-exit 1
 # run_maven -f spring-boot-project/pom.xml clean deploy -U -Dfull -DaltDeploymentRepository=distribution::default::file://${repository} -DskipTests
 popd > /dev/null
--- ci/scripts/handle-outcome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source $(dirname $0)/common.sh
-
-echo "Handle build outcome $1"
-
-git clone build-info-git-repo updated-build-info-git-repo
-echo "spring-boot-builds@mailinator.com" > email-notification/to
