f6a261673a96f80d551e00e7faa397e80f5a6916
Phillip Webb <pwebb@pivotal.io>
2017-09-25 14:28:08 -0700

    Hacking email

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,5 +5,6 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
+exit 1
 # run_maven -f spring-boot-project/pom.xml clean deploy -U -Dfull -DaltDeploymentRepository=distribution::default::file://${repository} -DskipTests
 popd > /dev/null
