317a376d79ded95f1efdc723d67c6cca7d29b642
Phillip Webb <pwebb@pivotal.io>
2017-12-21 10:20:07 -0800

    Drop __ support

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/source/SpringConfigurationPropertySource.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/source/SpringConfigurationPropertySource.java
@@ -40,7 +40,7 @@ import org.springframework.util.Assert;
  * <p>
  * Each {@link ConfigurationPropertySource#getConfigurationProperty
  * getConfigurationProperty} call attempts to
- * {@link PropertyMapper#map(PropertySource, ConfigurationPropertyName) map} the
+ * {@link PropertyMapper#map(ConfigurationPropertyName) map} the
  * {@link ConfigurationPropertyName} to one or more {@code String} based names. This
  * allows fast property resolution for well formed property sources.
  * <p>
@@ -86,7 +86,7 @@ class SpringConfigurationPropertySource implements ConfigurationPropertySource {
 	public ConfigurationProperty getConfigurationProperty(
 			ConfigurationPropertyName name) {
 		System.out.println(name);
-		List<PropertyMapping> mappings = getMapper().map(getPropertySource(), name);
+		List<PropertyMapping> mappings = getMapper().map(name);
 		return find(mappings, name);
 	}
 
@@ -216,10 +216,10 @@ class SpringConfigurationPropertySource implements ConfigurationPropertySource {
 		}
 
 		@Override
-		public List<PropertyMapping> map(PropertySource<?> propertySource,
+		public List<PropertyMapping> map(
 				ConfigurationPropertyName configurationPropertyName) {
 			try {
-				return this.mapper.map(propertySource, configurationPropertyName);
+				return this.mapper.map(configurationPropertyName);
 			}
 			catch (Exception ex) {
 				return Collections.emptyList();
@@ -227,10 +227,9 @@ class SpringConfigurationPropertySource implements ConfigurationPropertySource {
 		}
 
 		@Override
-		public List<PropertyMapping> map(PropertySource<?> propertySource,
-				String propertySourceName) {
+		public List<PropertyMapping> map(String propertySourceName) {
 			try {
-				return this.mapper.map(propertySource, propertySourceName);
+				return this.mapper.map(propertySourceName);
 			}
 			catch (Exception ex) {
 				return Collections.emptyList();
