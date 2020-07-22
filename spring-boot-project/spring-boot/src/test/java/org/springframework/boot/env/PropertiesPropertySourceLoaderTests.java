c1072c0ea86d7c86d9960bb64b6a7308e51ac60e
Phillip Webb <pwebb@vmware.com>
2020-07-21 20:11:17 -0700

    Multidoc

--- spring-boot-project/spring-boot/src/test/java/org/springframework/boot/env/PropertiesPropertySourceLoaderTests.java
+++ spring-boot-project/spring-boot/src/test/java/org/springframework/boot/env/PropertiesPropertySourceLoaderTests.java
@@ -49,6 +49,39 @@ class PropertiesPropertySourceLoaderTests {
 	}
 
 	@Test
+	void loadMultiDocumentPropertiesWithSeparatorAtTheBeginningofFile() throws Exception {
+		List<PropertySource<?>> loaded = this.loader.load("test.properties",
+				new ClassPathResource("multi-document-properties-2.properties", getClass()));
+		assertThat(loaded.size()).isEqualTo(2);
+		PropertySource<?> source1 = loaded.get(0);
+		PropertySource<?> source2 = loaded.get(1);
+		assertThat(source1.getProperty("blah")).isEqualTo("hello world");
+		assertThat(source2.getProperty("foo")).isEqualTo("bar");
+	}
+
+	@Test
+	void loadMultiDocumentProperties() throws Exception {
+		List<PropertySource<?>> loaded = this.loader.load("test.properties",
+				new ClassPathResource("multi-document-properties.properties", getClass()));
+		assertThat(loaded.size()).isEqualTo(2);
+		PropertySource<?> source1 = loaded.get(0);
+		PropertySource<?> source2 = loaded.get(1);
+		assertThat(source1.getProperty("blah")).isEqualTo("hello world");
+		assertThat(source2.getProperty("foo")).isEqualTo("bar");
+	}
+
+	@Test
+	void loadMultiDocumentPropertiesWithEmptyDocument() throws Exception {
+		List<PropertySource<?>> loaded = this.loader.load("test.properties",
+				new ClassPathResource("multi-document-properties-empty.properties", getClass()));
+		assertThat(loaded.size()).isEqualTo(2);
+		PropertySource<?> source1 = loaded.get(0);
+		PropertySource<?> source2 = loaded.get(1);
+		assertThat(source1.getProperty("blah")).isEqualTo("hello world");
+		assertThat(source2.getProperty("foo")).isEqualTo("bar");
+	}
+
+	@Test
 	void loadXml() throws Exception {
 		List<PropertySource<?>> loaded = this.loader.load("test.xml",
 				new ClassPathResource("test-xml.xml", getClass()));
