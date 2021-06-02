0b4970cdae1c08732b7226cb3c36ada422bf5c26
Phillip Webb <pwebb@vmware.com>
2021-06-02 16:29:00 -0700

    JarFileClose

--- spring-boot-project/spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/jar/JarFileWrapper.java
+++ spring-boot-project/spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/jar/JarFileWrapper.java
@@ -40,7 +40,9 @@ class JarFileWrapper extends AbstractJarFile {
 	JarFileWrapper(JarFile parent) throws IOException {
 		super(parent.getRootJarFile().getFile());
 		this.parent = parent;
-		super.close();
+		if (System.getSecurityManager() == null) {
+			super.close();
+		}
 	}
 
 	@Override
