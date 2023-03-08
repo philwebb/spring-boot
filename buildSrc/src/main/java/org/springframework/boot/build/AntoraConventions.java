bb25b26a568fb8cbc46ca7b95f142371acfbf0ac
Phillip Webb <pwebb@vmware.com>
2023-03-08 13:13:12 -0800

    Hack

--- buildSrc/src/main/java/org/springframework/boot/build/AntoraConventions.java
+++ buildSrc/src/main/java/org/springframework/boot/build/AntoraConventions.java
@@ -16,17 +16,23 @@
 
 package org.springframework.boot.build;
 
+import java.io.File;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import io.spring.gradle.antora.GenerateAntoraYmlPlugin;
 import io.spring.gradle.antora.GenerateAntoraYmlTask;
 import org.antora.gradle.AntoraExtension;
 import org.antora.gradle.AntoraPlugin;
+import org.antora.gradle.AntoraTask;
 import org.gradle.api.Project;
+import org.gradle.api.tasks.PathSensitivity;
+import org.gradle.api.tasks.Sync;
 
 import org.springframework.boot.build.artifacts.ArtifactRelease;
+import org.springframework.util.StringUtils;
 
 /**
  * Conventions that are applied in the presence of the {@link AntoraPlugin} and
@@ -52,26 +58,48 @@ public class AntoraConventions {
 	}
 
 	void apply(Project project) {
-		project.getPlugins().withType(AntoraPlugin.class, (plugin) -> configureAntoraPlugin(project, plugin));
 		project.getPlugins()
-			.withType(GenerateAntoraYmlPlugin.class, (plugin) -> configureGenerateAntoraYmlPlugin(project, plugin));
+			.withType(AntoraPlugin.class, (antoraPlugin) -> configureAntoraPlugin(project, antoraPlugin));
 	}
 
-	private void configureAntoraPlugin(Project project, AntoraPlugin plugin) {
+	private void configureAntoraPlugin(Project project, AntoraPlugin antoraPlugin) {
+		GenerateAntoraYmlPlugin generateAntoraYmlPlugin = project.getPlugins().getPlugin(GenerateAntoraYmlPlugin.class);
+		Objects.requireNonNull(generateAntoraYmlPlugin, "GenerateAntoraYmlPlugin has not been applied");
 		AntoraExtension extension = project.getExtensions().getByType(AntoraExtension.class);
 		extension.getVersion().convention(ANTORA_VERSION);
 		extension.getPackages().convention(PACKAGES);
+		project.getTasks().withType(AntoraTask.class, (antoraTask) -> configureAntoraTask(project, antoraTask));
+		configureGenerateAntoraYmlPlugin(project, generateAntoraYmlPlugin);
 	}
 
-	private void configureGenerateAntoraYmlPlugin(Project project, GenerateAntoraYmlPlugin plugin) {
+	private void configureAntoraTask(Project project, AntoraTask antoraTask) {
+		createSyncContentTask(project, antoraTask);
+	}
+
+	private Sync createSyncContentTask(Project project, AntoraTask antoraTask) {
+		String taskName = "sync" + StringUtils.capitalize(antoraTask.getName()) + "Content";
+		Sync syncTask = project.getTasks().create(taskName, Sync.class);
+		File destination = new File(project.getBuildDir(), "generated/antora");
+		syncTask.setDestinationDir(destination);
+		project.getTasks().withType(GenerateAntoraYmlTask.class).forEach(syncTask::from);
+		antoraTask.dependsOn(syncTask);
+		antoraTask.getInputs()
+			.dir(destination)
+			.withPathSensitivity(PathSensitivity.RELATIVE)
+			.withPropertyName("synced source");
+		return syncTask;
+	}
+
+	private void configureGenerateAntoraYmlPlugin(Project project, GenerateAntoraYmlPlugin generateAntoraYmlPlugin) {
 		project.getTasks()
-			.withType(GenerateAntoraYmlTask.class, (task) -> configureGenerateAntoraYmlTask(project, task));
+			.withType(GenerateAntoraYmlTask.class,
+					(generateAntoraYmlTask) -> configureGenerateAntoraYmlTask(project, generateAntoraYmlTask));
 	}
 
-	private void configureGenerateAntoraYmlTask(Project project, GenerateAntoraYmlTask task) {
-		task.setProperty("baseAntoraYmlFile", project.file("src/docs/antora/antora.yml"));
-		task.setProperty("outputFile", project.file("$buildDir/generated/antora/antora.yml"));
-		task.doFirst((actionedTask) -> task.getAsciidocAttributes()
+	private void configureGenerateAntoraYmlTask(Project project, GenerateAntoraYmlTask generateAntoraYmlTask) {
+		generateAntoraYmlTask.setProperty("baseAntoraYmlFile", project.file("src/docs/antora/antora.yml"));
+		generateAntoraYmlTask.setProperty("outputFile", new File(project.getBuildDir(), "generated/antora/antora.yml"));
+		generateAntoraYmlTask.doFirst((actionedTask) -> generateAntoraYmlTask.getAsciidocAttributes()
 			.putAll(project.provider(() -> provideAsciidocAttributes(project))));
 	}
 
