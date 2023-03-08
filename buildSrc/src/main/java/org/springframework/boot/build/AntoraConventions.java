9d8df3367f8f6397c61e7f43741e6754b30da5ab
Phillip Webb <pwebb@vmware.com>
2023-03-07 19:04:30 -0800

    Hacking

--- buildSrc/src/main/java/org/springframework/boot/build/AntoraConventions.java
+++ buildSrc/src/main/java/org/springframework/boot/build/AntoraConventions.java
@@ -16,7 +16,12 @@
 
 package org.springframework.boot.build;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import io.spring.gradle.antora.GenerateAntoraYmlPlugin;
+import org.antora.gradle.AntoraExtension;
 import org.antora.gradle.AntoraPlugin;
 import org.gradle.api.Project;
 
@@ -28,7 +33,33 @@ import org.gradle.api.Project;
  */
 public class AntoraConventions {
 
+	private static final String ANTORA_VERSION = "3.2.0-alpha.2";
+
+	private static final Map<String, String> PACKAGES;
+	static {
+		Map<String, String> packages = new LinkedHashMap<>();
+		packages.put("@antora/atlas-extension", "1.0.0-alpha.1");
+		packages.put("@antora/collector-extension", "1.0.0-alpha.2");
+		packages.put("@antora/collector-extension", "1.0.0-alpha.3");
+		packages.put("@asciidoctor/tabs", "1.0.0-alpha.12");
+		packages.put("@opendevise/antora-release-line-extension", "1.0.0-alpha.2");
+		packages.put("@springio/antora-extensions", "1.1.0");
+		packages.put("@springio/asciidoctor-extensions", "1.0.0-alpha.9");
+		PACKAGES = Collections.unmodifiableMap(packages);
+	}
+
 	void apply(Project project) {
+		project.getPlugins().withType(AntoraPlugin.class, (plugin) -> apply(project, plugin));
+		project.getPlugins().withType(GenerateAntoraYmlPlugin.class, (plugin) -> apply(project, plugin));
+	}
+
+	private void apply(Project project, AntoraPlugin plugin) {
+		AntoraExtension antoraExtension = project.getExtensions().getByType(AntoraExtension.class);
+		antoraExtension.getVersion().convention(ANTORA_VERSION);
+		antoraExtension.getPackages().convention(PACKAGES);
+	}
+
+	private void apply(Project project, GenerateAntoraYmlPlugin plugin) {
 	}
 
 }
