1bfb1c55c89f94a8af500dc2ea76767d672bf01c
Moritz Halbritter <moritz.halbritter@broadcom.com>
2024-02-02 15:03:49 +0100

    Optionally include pid, service name and version in ECS format

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/logback/JsonEncoder.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/logback/JsonEncoder.java
@@ -17,7 +17,6 @@
 package org.springframework.boot.logging.logback;
 
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,8 +30,8 @@ import ch.qos.logback.core.encoder.Encoder;
 import ch.qos.logback.core.encoder.EncoderBase;
 import org.slf4j.event.KeyValuePair;
 
-import org.springframework.boot.logging.logback.JsonEncoder.Format.Context;
-import org.springframework.boot.logging.logback.JsonEncoder.Format.Field;
+import org.springframework.boot.logging.logback.JsonEncoder.JsonFormat.Context;
+import org.springframework.boot.logging.logback.JsonEncoder.JsonFormat.Field;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
@@ -45,13 +44,17 @@ class JsonEncoder extends EncoderBase<ILoggingEvent> {
 
 	private final ThrowableProxyConverter throwableProxyConverter = new ThrowableProxyConverter();
 
-	private final Format format;
+	private final JsonFormat format;
 
 	private final Context formatContext;
 
-	JsonEncoder(Format format) {
+	JsonEncoder(JsonFormat format) {
+		this(format, null, null, null);
+	}
+
+	JsonEncoder(JsonFormat format, Long pid, String serviceName, String serviceVersion) {
 		this.format = format;
-		this.formatContext = new Context(this.throwableProxyConverter);
+		this.formatContext = new Context(pid, serviceName, serviceVersion, this.throwableProxyConverter);
 	}
 
 	@Override
@@ -76,63 +79,13 @@ class JsonEncoder extends EncoderBase<ILoggingEvent> {
 		StringBuilder output = new StringBuilder();
 		output.append('{');
 		for (Field field : this.format.getFields(this.formatContext, event)) {
-			writeField(field, output);
+			field.write(output);
+			output.append(',');
 		}
 		removeTrailingComma(output);
 		output.append('}');
 		output.append('\n');
 		return output.toString().getBytes(StandardCharsets.UTF_8);
-	}
-
-	private void writeField(Field field, StringBuilder output) {
-		if (field.escapeKey()) {
-			writeKey(field.key(), output);
-		}
-		else {
-			writeVerbatimKey(field.key(), output);
-		}
-		output.append(':');
-		if (field.escapeValue()) {
-			writeValue(field.value(), output);
-		}
-		else {
-			writeVerbatimValue(field.value(), output);
-		}
-		output.append(',');
-	}
-
-	private void writeValue(String value, StringBuilder output) {
-		output.append('\"');
-		if (value == null) {
-			output.append("null");
-		}
-		else {
-			escape(value, output);
-		}
-		output.append("\"");
-	}
-
-	private void writeVerbatimValue(String value, StringBuilder output) {
-		output.append('\"');
-		output.append(value);
-		output.append('\"');
-	}
-
-	private void writeKey(String key, StringBuilder output) {
-		output.append('\"');
-		if (key == null) {
-			output.append("null");
-		}
-		else {
-			escape(key, output);
-		}
-		output.append("\"");
-	}
-
-	private void writeVerbatimKey(String key, StringBuilder output) {
-		output.append('\"');
-		output.append(key);
-		output.append('\"');
 	}
 
 	private void removeTrailingComma(StringBuilder output) {
@@ -143,78 +96,150 @@ class JsonEncoder extends EncoderBase<ILoggingEvent> {
 		}
 	}
 
-	private void escape(String text, StringBuilder output) {
-		for (int i = 0; i < text.length(); i++) {
-			char c = text.charAt(i);
-			escape(c, output);
-		}
-	}
-
-	private void escape(char c, StringBuilder output) {
-		// TODO: More JSON \\u escaping, see
-		// co.elastic.logging.JsonUtils#quoteAsString(java.lang.CharSequence, int, int,
-		// java.lang.StringBuilder)
-		switch (c) {
-			case '\b' -> output.append("\\b");
-			case '\t' -> output.append("\\t");
-			case '\f' -> output.append("\\f");
-			case '\n' -> output.append("\\n");
-			case '\r' -> output.append("\\r");
-			case '"' -> output.append("\\\"");
-			case '\\' -> output.append("\\\\");
-			default -> output.append(c);
-		}
-	}
-
 	@Override
 	public byte[] footerBytes() {
 		return null;
 	}
 
-	interface Format {
+	interface JsonFormat {
 
 		Iterable<Field> getFields(Context context, ILoggingEvent event);
 
-		record Field(String key, String value, boolean escapeKey, boolean escapeValue) {
-			static Field escaped(String key, String value) {
-				return new Field(key, value, true, true);
-			}
-
-			static Field escapedValue(String key, String value) {
-				return new Field(key, value, false, true);
-			}
-
-			static Field verbatim(String key, String value) {
-				return new Field(key, value, false, false);
-			}
+		record Context(Long pid, String serviceName, String serviceVersion,
+				ThrowableProxyConverter throwableProxyConverter) {
 		}
 
-		record Context(ThrowableProxyConverter throwableProxyConverter) {
+		interface Field {
+
+			void write(StringBuilder output);
+
 		}
 
 	}
 
-	enum CommonFormats implements Format {
+	static final class StandardField implements JsonFormat.Field {
+
+		private final Key key;
+
+		private final Value value;
+
+		private StandardField(Key key, Value value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		@Override
+		public void write(StringBuilder output) {
+			output.append('\"');
+			this.key.write(output);
+			output.append("\":");
+			this.value.write(output);
+		}
+
+		static StandardField of(Key key, Value value) {
+			return new StandardField(key, value);
+		}
+
+	}
+
+	interface Key {
+
+		void write(StringBuilder output);
+
+		static Key verbatim(String key) {
+			return (output) -> output.append(key);
+		}
+
+		static Key escaped(String key) {
+			return (output) -> JsonHelper.escape(key, output);
+		}
+
+	}
+
+	interface Value {
+
+		void write(StringBuilder output);
+
+		static Value verbatim(String value) {
+			return (output) -> output.append('\"').append(value).append('\"');
+		}
+
+		static Value escaped(String value) {
+			return (output) -> {
+				output.append('\"');
+				JsonHelper.escape(value, output);
+				output.append('\"');
+			};
+		}
+
+		static Value of(long value) {
+			return (output) -> output.append(value);
+		}
+
+	}
+
+	private static final class JsonHelper {
+
+		private JsonHelper() {
+		}
+
+		static void escape(String text, StringBuilder output) {
+			for (int i = 0; i < text.length(); i++) {
+				char c = text.charAt(i);
+				escape(c, output);
+			}
+		}
+
+		static void escape(char c, StringBuilder output) {
+			// TODO: More JSON \\u escaping, see
+			// co.elastic.logging.JsonUtils#quoteAsString(java.lang.CharSequence, int,
+			// int,
+			// java.lang.StringBuilder)
+			switch (c) {
+				case '\b' -> output.append("\\b");
+				case '\t' -> output.append("\\t");
+				case '\f' -> output.append("\\f");
+				case '\n' -> output.append("\\n");
+				case '\r' -> output.append("\\r");
+				case '"' -> output.append("\\\"");
+				case '\\' -> output.append("\\\\");
+				default -> output.append(c);
+			}
+		}
+
+	}
+
+	enum CommonFormats implements JsonFormat {
 
 		ECS {
 			@Override
 			public Iterable<Field> getFields(Context context, ILoggingEvent event) {
 				List<Field> fields = new ArrayList<>();
-				fields.add(Field.verbatim("@timestamp", Instant.ofEpochMilli(event.getTimeStamp()).toString()));
-				fields.add(Field.verbatim("log.level", event.getLevel().toString()));
-				fields.add(Field.escapedValue("message", event.getFormattedMessage()));
-				fields.add(Field.verbatim("ecs.version", "1.2.0"));
-				fields.add(Field.escapedValue("process.thread.name", event.getThreadName()));
-				fields.add(Field.escapedValue("log.logger", event.getLoggerName()));
+				fields.add(StandardField.of(Key.verbatim("@timestamp"), Value.verbatim(event.getInstant().toString())));
+				fields.add(StandardField.of(Key.verbatim("log.level"), Value.verbatim(event.getLevel().toString())));
+				if (context.pid() != null) {
+					fields.add(StandardField.of(Key.verbatim("process.pid"), Value.of(context.pid())));
+				}
+				fields.add(StandardField.of(Key.verbatim("process.thread.name"), Value.escaped(event.getThreadName())));
+				if (context.serviceName() != null) {
+					fields.add(StandardField.of(Key.verbatim("service.name"), Value.escaped(context.serviceName())));
+				}
+				if (context.serviceVersion() != null) {
+					fields.add(
+							StandardField.of(Key.verbatim("service.version"), Value.escaped(context.serviceVersion())));
+				}
+				fields.add(StandardField.of(Key.verbatim("log.logger"), Value.escaped(event.getLoggerName())));
+				fields.add(StandardField.of(Key.verbatim("message"), Value.escaped(event.getFormattedMessage())));
 				addMdc(event, fields);
 				addKeyValuePairs(event, fields);
 				IThrowableProxy throwable = event.getThrowableProxy();
 				if (throwable != null) {
-					fields.add(Field.verbatim("error.type", throwable.getClassName()));
-					fields.add(Field.escapedValue("error.message", throwable.getMessage()));
-					fields
-						.add(Field.escapedValue("error.stack_trace", context.throwableProxyConverter().convert(event)));
+					fields.add(StandardField.of(Key.verbatim("error.type"), Value.verbatim(throwable.getClassName())));
+					fields.add(StandardField.of(Key.verbatim("error.message"), Value.escaped(throwable.getMessage())));
+					fields.add(StandardField.of(Key.verbatim("error.stack_trace"),
+							Value.escaped(context.throwableProxyConverter().convert(event))));
 				}
+				fields.add(StandardField.of(Key.verbatim("ecs.version"), Value.verbatim("1.2.0")));
 				return fields;
 				// TODO: Service name, service version, service env, service node name,
 				// event dataset
@@ -226,7 +251,8 @@ class JsonEncoder extends EncoderBase<ILoggingEvent> {
 					return;
 				}
 				for (KeyValuePair keyValuePair : keyValuePairs) {
-					fields.add(Field.escaped(keyValuePair.key, ObjectUtils.nullSafeToString(keyValuePair.value)));
+					fields.add(StandardField.of(Key.escaped(keyValuePair.key),
+							Value.escaped(ObjectUtils.nullSafeToString(keyValuePair.value))));
 				}
 			}
 
@@ -236,12 +262,12 @@ class JsonEncoder extends EncoderBase<ILoggingEvent> {
 					return;
 				}
 				for (Entry<String, String> entry : mdc.entrySet()) {
-					fields.add(Field.escaped(entry.getKey(), entry.getValue()));
+					fields.add(StandardField.of(Key.escaped(entry.getKey()), Value.escaped(entry.getValue())));
 				}
 			}
 		};
 
-		static Format parse(String input) {
+		static JsonFormat parse(String input) {
 			for (CommonFormats value : values()) {
 				if (value.name().equalsIgnoreCase(input)) {
 					return value;
