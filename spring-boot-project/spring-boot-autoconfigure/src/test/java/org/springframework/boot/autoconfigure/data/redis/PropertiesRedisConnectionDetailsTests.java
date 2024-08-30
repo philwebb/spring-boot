6ac5ef0bef562e5b4203ac8931db9b2264cf49a8
Moritz Halbritter <moritz.halbritter@broadcom.com>
2024-08-30 11:14:02 +0200

    Improve PropertiesRedisConnectionDetailsTests

--- spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/redis/PropertiesRedisConnectionDetailsTests.java
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/data/redis/PropertiesRedisConnectionDetailsTests.java
@@ -18,42 +18,54 @@ package org.springframework.boot.autoconfigure.data.redis;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.autoconfigure.data.redis.RedisConnectionDetails.Node;
+import org.springframework.boot.ssl.DefaultSslBundleRegistry;
+import org.springframework.boot.ssl.SslBundle;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  * Tests for {@link PropertiesRedisConnectionDetails}.
  *
  * @author Scott Frederick
+ * @author Moritz Halbritter
  */
 class PropertiesRedisConnectionDetailsTests {
 
-	private final RedisProperties properties = new RedisProperties();
+	private RedisProperties properties;
+
+	private PropertiesRedisConnectionDetails connectionDetails;
+
+	private DefaultSslBundleRegistry sslBundleRegistry;
+
+	@BeforeEach
+	void setUp() {
+		this.properties = new RedisProperties();
+		this.sslBundleRegistry = new DefaultSslBundleRegistry();
+		this.connectionDetails = new PropertiesRedisConnectionDetails(this.properties, this.sslBundleRegistry);
+	}
 
 	@Test
 	void connectionIsConfiguredWithDefaults() {
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		RedisConnectionDetails.Standalone standalone = connectionDetails.getStandalone();
+		RedisConnectionDetails.Standalone standalone = this.connectionDetails.getStandalone();
 		assertThat(standalone.getHost()).isEqualTo("localhost");
 		assertThat(standalone.getPort()).isEqualTo(6379);
 		assertThat(standalone.getDatabase()).isEqualTo(0);
-		assertThat(connectionDetails.getSentinel()).isNull();
-		assertThat(connectionDetails.getCluster()).isNull();
-		assertThat(connectionDetails.getUsername()).isNull();
-		assertThat(connectionDetails.getPassword()).isNull();
+		assertThat(this.connectionDetails.getSentinel()).isNull();
+		assertThat(this.connectionDetails.getCluster()).isNull();
+		assertThat(this.connectionDetails.getUsername()).isNull();
+		assertThat(this.connectionDetails.getPassword()).isNull();
 	}
 
 	@Test
 	void credentialsAreConfiguredFromUrlWithUsernameAndPassword() {
 		this.properties.setUrl("redis://user:secret@example.com");
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		assertThat(connectionDetails.getUsername()).isEqualTo("user");
-		assertThat(connectionDetails.getPassword()).isEqualTo("secret");
+		assertThat(this.connectionDetails.getUsername()).isEqualTo("user");
+		assertThat(this.connectionDetails.getPassword()).isEqualTo("secret");
 	}
 
 	@Test
@@ -61,10 +73,8 @@ class PropertiesRedisConnectionDetailsTests {
 		this.properties.setUrl("redis://user:@example.com");
 		this.properties.setUsername("notused");
 		this.properties.setPassword("notused");
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		assertThat(connectionDetails.getUsername()).isEqualTo("user");
-		assertThat(connectionDetails.getPassword()).isEmpty();
+		assertThat(this.connectionDetails.getUsername()).isEqualTo("user");
+		assertThat(this.connectionDetails.getPassword()).isEmpty();
 	}
 
 	@Test
@@ -72,10 +82,8 @@ class PropertiesRedisConnectionDetailsTests {
 		this.properties.setUrl("redis://:secret@example.com");
 		this.properties.setUsername("notused");
 		this.properties.setPassword("notused");
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		assertThat(connectionDetails.getUsername()).isEmpty();
-		assertThat(connectionDetails.getPassword()).isEqualTo("secret");
+		assertThat(this.connectionDetails.getUsername()).isEmpty();
+		assertThat(this.connectionDetails.getPassword()).isEqualTo("secret");
 	}
 
 	@Test
@@ -83,20 +91,16 @@ class PropertiesRedisConnectionDetailsTests {
 		this.properties.setUrl("redis://secret@example.com");
 		this.properties.setUsername("notused");
 		this.properties.setPassword("notused");
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		assertThat(connectionDetails.getUsername()).isNull();
-		assertThat(connectionDetails.getPassword()).isEqualTo("secret");
+		assertThat(this.connectionDetails.getUsername()).isNull();
+		assertThat(this.connectionDetails.getPassword()).isEqualTo("secret");
 	}
 
 	@Test
 	void credentialsAreConfiguredFromProperties() {
 		this.properties.setUsername("user");
 		this.properties.setPassword("secret");
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		assertThat(connectionDetails.getUsername()).isEqualTo("user");
-		assertThat(connectionDetails.getPassword()).isEqualTo("secret");
+		assertThat(this.connectionDetails.getUsername()).isEqualTo("user");
+		assertThat(this.connectionDetails.getPassword()).isEqualTo("secret");
 	}
 
 	@Test
@@ -105,9 +109,7 @@ class PropertiesRedisConnectionDetailsTests {
 		this.properties.setHost("notused");
 		this.properties.setPort(9999);
 		this.properties.setDatabase(5);
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		RedisConnectionDetails.Standalone standalone = connectionDetails.getStandalone();
+		RedisConnectionDetails.Standalone standalone = this.connectionDetails.getStandalone();
 		assertThat(standalone.getHost()).isEqualTo("example.com");
 		assertThat(standalone.getPort()).isEqualTo(1234);
 		assertThat(standalone.getDatabase()).isEqualTo(5);
@@ -118,9 +120,7 @@ class PropertiesRedisConnectionDetailsTests {
 		this.properties.setHost("example.com");
 		this.properties.setPort(1234);
 		this.properties.setDatabase(5);
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		RedisConnectionDetails.Standalone standalone = connectionDetails.getStandalone();
+		RedisConnectionDetails.Standalone standalone = this.connectionDetails.getStandalone();
 		assertThat(standalone.getHost()).isEqualTo("example.com");
 		assertThat(standalone.getPort()).isEqualTo(1234);
 		assertThat(standalone.getDatabase()).isEqualTo(5);
@@ -131,9 +131,7 @@ class PropertiesRedisConnectionDetailsTests {
 		RedisProperties.Cluster cluster = new RedisProperties.Cluster();
 		cluster.setNodes(List.of("localhost:1111", "127.0.0.1:2222", "[::1]:3333"));
 		this.properties.setCluster(cluster);
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		assertThat(connectionDetails.getCluster().getNodes()).containsExactly(new Node("localhost", 1111),
+		assertThat(this.connectionDetails.getCluster().getNodes()).containsExactly(new Node("localhost", 1111),
 				new Node("127.0.0.1", 2222), new Node("[::1]", 3333));
 	}
 
@@ -142,10 +140,31 @@ class PropertiesRedisConnectionDetailsTests {
 		RedisProperties.Sentinel sentinel = new RedisProperties.Sentinel();
 		sentinel.setNodes(List.of("localhost:1111", "127.0.0.1:2222", "[::1]:3333"));
 		this.properties.setSentinel(sentinel);
-		PropertiesRedisConnectionDetails connectionDetails = new PropertiesRedisConnectionDetails(this.properties,
-				null);
-		assertThat(connectionDetails.getSentinel().getNodes()).containsExactly(new Node("localhost", 1111),
+		assertThat(this.connectionDetails.getSentinel().getNodes()).containsExactly(new Node("localhost", 1111),
 				new Node("127.0.0.1", 2222), new Node("[::1]", 3333));
+	}
+
+	@Test
+	void shouldReturnSslBundle() {
+		SslBundle bundle1 = mock(SslBundle.class);
+		this.sslBundleRegistry.registerBundle("bundle-1", bundle1);
+		this.properties.getSsl().setBundle("bundle-1");
+		SslBundle sslBundle = this.connectionDetails.getStandalone().getSslBundle();
+		assertThat(sslBundle).isSameAs(bundle1);
+	}
+
+	@Test
+	void shouldReturnSystemBundleIfSslIsEnabledButBundleNotSet() {
+		this.properties.getSsl().setEnabled(true);
+		SslBundle sslBundle = this.connectionDetails.getStandalone().getSslBundle();
+		assertThat(sslBundle).isNotNull();
+	}
+
+	@Test
+	void shouldReturnNullIfSslIsNotEnabled() {
+		this.properties.getSsl().setEnabled(false);
+		SslBundle sslBundle = this.connectionDetails.getStandalone().getSslBundle();
+		assertThat(sslBundle).isNull();
 	}
 
 }
