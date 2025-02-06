90c8655f11bbb371e21e56fb91a2837c375e99e8
Phillip Webb <phil.webb@broadcom.com>
2025-02-06 13:11:58 -0800

    Hacking

---
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/StandardStackTracePrinter.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.logging;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
+/**
+ * {@link StackTracePrinter} that prints a stack trace in a standard form. This printer
+ * can produce a result in a similar form to {@link Throwable#printStackTrace()}, but
+ * offers many more customization options.
+ *
+ * @author Phillip Webb
+ * @since 3.5.0
+ */
+public final class StandardStackTracePrinter implements StackTracePrinter {
+
+	private static final String CAUSE_CAPTION = "Caused by: ";
+
+	private static final String SUPPRESSED_CAPTION = "Suppressed: ";
+
+	@Override
+	public void printStackTrace(Throwable throwable, Appendable out) throws IOException {
+		Set<Throwable> dejaVu = Collections.newSetFromMap(new IdentityHashMap<>());
+		dejaVu.add(throwable);
+		println(out, throwable.toString());
+		StackTraceElement[] trace = throwable.getStackTrace();
+		for (StackTraceElement traceElement : trace) {
+			println(out, "\tat " + traceElement);
+		}
+		for (Throwable se : throwable.getSuppressed()) {
+			printEnclosedStackTrace(se, out, trace, SUPPRESSED_CAPTION, "\t", dejaVu);
+		}
+		Throwable ourCause = throwable.getCause();
+		if (ourCause != null) {
+			printEnclosedStackTrace(ourCause, out, trace, CAUSE_CAPTION, "", dejaVu);
+		}
+	}
+
+	private void printEnclosedStackTrace(Throwable dis, Appendable out, StackTraceElement[] enclosingTrace,
+			String caption, String prefix, Set<Throwable> dejaVu) throws IOException {
+		if (dejaVu.contains(dis)) {
+			out.append(prefix + caption + "[CIRCULAR REFERENCE: " + this + "]");
+			out.append("\n");
+		}
+		else {
+			dejaVu.add(dis);
+			StackTraceElement[] trace = dis.getStackTrace();
+			int m = trace.length - 1;
+			int n = enclosingTrace.length - 1;
+			while (m >= 0 && n >= 0 && trace[m].equals(enclosingTrace[n])) {
+				m--;
+				n--;
+			}
+			int framesInCommon = trace.length - 1 - m;
+			println(out, prefix + caption + dis);
+			for (int i = 0; i <= m; i++) {
+				println(out, prefix + "\tat " + trace[i]);
+			}
+			if (framesInCommon != 0) {
+				println(out, prefix + "\t... " + framesInCommon + " more");
+			}
+			for (Throwable se : dis.getSuppressed()) {
+				printEnclosedStackTrace(se, out, trace, SUPPRESSED_CAPTION, prefix + "\t", dejaVu);
+			}
+			Throwable ourCause = dis.getCause();
+			if (ourCause != null) {
+				printEnclosedStackTrace(ourCause, out, trace, CAUSE_CAPTION, prefix, dejaVu);
+			}
+		}
+	}
+
+	private void println(Appendable out, String string) throws IOException {
+		out.append(string);
+		out.append("\n");
+	}
+
+	public StandardStackTracePrinter withMaximumLength(int maximumLength) {
+		return this;
+	}
+
+	public StandardStackTracePrinter withMaximumThrowableDepth(int maximumThrowableDepth) {
+		return this;
+	}
+
+	public StandardStackTracePrinter withFilter(Predicate<Throwable> predicate) {
+		return this;
+	}
+
+	public StandardStackTracePrinter withElementFilter(Predicate<StackTraceElement> predicate) {
+		return this;
+	}
+
+	public StandardStackTracePrinter withClassNameFormatter(UnaryOperator<String> classNameFormatter) {
+		return this;
+	}
+
+	// Printer?
+
+	public StandardStackTracePrinter with() {
+		return this;
+	}
+
+	/**
+	 * Return a {@link StandardStackTracePrinter} that prints the stack trace with the
+	 * root exception last (the same as {@link Throwable#printStackTrace()}).
+	 * @return a {@link StandardStackTracePrinter} that prints the stack trace root last
+	 */
+	public static StandardStackTracePrinter rootLast() {
+		return new StandardStackTracePrinter();
+	}
+
+	/**
+	 * Return a {@link StandardStackTracePrinter} that prints the stack trace with the
+	 * root exception first (the opposite of {@link Throwable#printStackTrace()}).
+	 * @return a {@link StandardStackTracePrinter} that prints the stack trace root first
+	 */
+	public static StandardStackTracePrinter rootFirst() {
+		return new StandardStackTracePrinter();
+	}
+
+}
---
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/temp/SeparatorParser.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2013-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.boot.logging.temp;
+
+public class SeparatorParser {
+
+	private SeparatorParser() {
+		// utility class
+	}
+
+	/**
+	 * Parses the given separator string.
+	 * 
+	 * The following values have special meaning:
+	 * <ul>
+	 * <li>{@code null} or empty string = no separator.</li>
+	 * <li>"{@code SYSTEM}}" = operating system new line.</li>
+	 * <li>"{@code UNIX}" = unix line ending ({@code \n}).</li>
+	 * <li>"{@code WINDOWS}" = windows line ending ({@code \r\n}).</li>
+	 * </ul>
+	 * <p>
+	 * Any other value will be returned as-is.
+	 * @param separator the separator format
+	 * @return the actual separator string after parsing
+	 */
+	public static String parseSeparator(String separator) {
+		if (separator == null || separator.isEmpty()) {
+			return null;
+		}
+		if (separator.equalsIgnoreCase("SYSTEM")) {
+			return System.lineSeparator();
+		}
+		if (separator.equalsIgnoreCase("UNIX")) {
+			return "\n";
+		}
+		if (separator.equalsIgnoreCase("WINDOWS")) {
+			return "\r\n";
+		}
+		return separator;
+	}
+
+}
