ade5b170eb68ed19981ee78d4826e25c85ee8c1c
Phillip Webb <phil.webb@broadcom.com>
2025-03-05 09:40:01 -0500

    Refactor `HttpInterfaceClientGroupProperties` to use base properties
    Refactor `HttpInterfaceClientGroupProperties` to use the new base
    properties object to remove common properties and leave only
    interface client properties.

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/interfaceclients/http/HttpInterfaceClientGroupProperties.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/interfaceclients/http/HttpInterfaceClientGroupProperties.java
@@ -16,14 +16,11 @@
 
 package org.springframework.boot.autoconfigure.interfaceclients.http;
 
-import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.boot.autoconfigure.http.client.DefaultHttpClientProperties;
 import org.springframework.boot.autoconfigure.http.client.HttpClientProperties;
-import org.springframework.boot.http.client.ClientHttpRequestFactorySettings.Redirects;
 
 /**
  * Properties for HTTP Interface Client Groups. Contains group registration properties and
@@ -32,7 +29,7 @@ import org.springframework.boot.http.client.ClientHttpRequestFactorySettings.Red
  * @author Olga Maciaszek-Sharma
  * @since 4.0.0
  */
-public class HttpInterfaceClientGroupProperties {
+public class HttpInterfaceClientGroupProperties extends HttpClientProperties {
 
 	// FIXME: use for adding clients
 
@@ -52,35 +49,12 @@ public class HttpInterfaceClientGroupProperties {
 	// making name obligatory to avoid url being a key
 
 	/**
-	 * Default request connect timeout for interface client group. By default, set to
-	 * {@code null}.
-	 */
-	private Duration connectTimeout = null;
-
-	/**
-	 * Default request read timeout for interface client group. By default, set to
-	 * {@code null}.
-	 */
-	private Duration readTimeout = null;
-
-	/**
 	 * Default request headers for interface client group. By default, set to empty
 	 * {@link Map}.
 	 */
 	private Map<String, List<String>> defaultHeaders = Collections.emptyMap();
 
 	// TODO: add implementation
-
-	/**
-	 * Handling for HTTP redirects. By default, set to
-	 * {@link Redirects#FOLLOW_WHEN_POSSIBLE}
-	 */
-	private Redirects redirects = Redirects.FOLLOW_WHEN_POSSIBLE;
-
-	/**
-	 * Default SSL configuration for a client HTTP request.
-	 */
-	private final HttpClientProperties.Ssl ssl = new HttpClientProperties.Ssl();
 
 	public String getBaseUrl() {
 		return this.baseUrl;
@@ -98,60 +72,12 @@ public class HttpInterfaceClientGroupProperties {
 		this.name = name;
 	}
 
-	public Duration getConnectTimeout() {
-		return this.connectTimeout;
-	}
-
-	public void setConnectTimeout(Duration connectTimeout) {
-		this.connectTimeout = connectTimeout;
-	}
-
-	public Duration getReadTimeout() {
-		return this.readTimeout;
-	}
-
-	public void setReadTimeout(Duration readTimeout) {
-		this.readTimeout = readTimeout;
-	}
-
 	public Map<String, List<String>> getDefaultHeaders() {
 		return this.defaultHeaders;
 	}
 
 	public void setDefaultHeaders(Map<String, List<String>> defaultHeaders) {
 		this.defaultHeaders = defaultHeaders;
-	}
-
-	public Redirects getRedirects() {
-		return this.redirects;
-	}
-
-	public void setRedirects(Redirects redirects) {
-		this.redirects = redirects;
-	}
-
-	public DefaultHttpClientProperties.Ssl getSsl() {
-		return this.ssl;
-	}
-
-	/**
-	 * SSL configuration.
-	 */
-	public static class Ssl {
-
-		/**
-		 * SSL bundle to use.
-		 */
-		private String bundle;
-
-		public String getBundle() {
-			return this.bundle;
-		}
-
-		public void setBundle(String bundle) {
-			this.bundle = bundle;
-		}
-
 	}
 
 }
