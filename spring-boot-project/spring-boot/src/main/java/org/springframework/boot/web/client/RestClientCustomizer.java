63fb1ed5fb3137ad31722edeab3dba2551707696
Phillip Webb <phil.webb@broadcom.com>
2025-03-06 23:12:55 -0500

    Make `RestClientCustomizer` a `Selectable`
    Update `RestClientCustomizer` so that it can be used as a fluent
    API to select specific `RestClient` instances.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/client/RestClientCustomizer.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/client/RestClientCustomizer.java
@@ -16,17 +16,23 @@
 
 package org.springframework.boot.web.client;
 
+import java.util.function.Predicate;
+
+import org.springframework.boot.selector.Selectable;
+import org.springframework.boot.selector.Selector;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClient.Builder;
 
 /**
  * Callback interface that can be used to customize a
  * {@link org.springframework.web.client.RestClient.Builder RestClient.Builder}.
  *
  * @author Arjen Poutsma
+ * @author Phillip Webb
  * @since 3.2.0
  */
 @FunctionalInterface
-public interface RestClientCustomizer {
+public interface RestClientCustomizer extends Selector<RestClientCustomizer> {
 
 	/**
 	 * Callback to customize a {@link org.springframework.web.client.RestClient.Builder
@@ -34,5 +40,66 @@ public interface RestClientCustomizer {
 	 * @param restClientBuilder the client builder to customize
 	 */
 	void customize(RestClient.Builder restClientBuilder);
+
+	/**
+	 * Callback to customize a {@link org.springframework.web.client.RestClient.Builder
+	 * RestClient.Builder} instance for a selected {@link Selectable}.
+	 * @param restClientBuilder the client builder to customize
+	 * @param selected the {@link #selects(Selectable) selected} selectable
+	 */
+	default void customize(RestClient.Builder restClientBuilder, Selectable selected) {
+		customize(restClientBuilder);
+	}
+
+	@Override
+	default RestClientCustomizer onlyWhen(Predicate<Selectable> predicate) {
+		return new ForSelected() {
+
+			@Override
+			public boolean selects(Selectable selectable) {
+				return (selectable != null) && RestClientCustomizer.this.selects(selectable)
+						&& predicate.test(selectable);
+			}
+
+			@Override
+			public void customize(RestClient.Builder restClientBuilder, Selectable selectable) {
+				RestClientCustomizer.this.customize(restClientBuilder, selectable);
+			}
+
+		};
+	}
+
+	/**
+	 * Helper method that can be used to create a {@link RestClientCustomizer} from a
+	 * lambda for method chaining.
+	 * @param customizer the source customizer
+	 * @return the customizer
+	 */
+	static RestClientCustomizer of(RestClientCustomizer customizer) {
+		return customizer;
+	}
+
+	/**
+	 * Helper method that can be used to create a {@link RestClientCustomizer} from a
+	 * lambda for method chaining.
+	 * @param customizer the source customizer
+	 * @return the customizer
+	 */
+	static RestClientCustomizer of(ForSelected customizer) {
+		return customizer;
+	}
+
+	/**
+	 * {@link RestClientCustomizer} that can be used when the selected {@link Selectable}
+	 * is needed during customization`.
+	 */
+	interface ForSelected extends RestClientCustomizer {
+
+		@Override
+		default void customize(Builder restClientBuilder) {
+			customize(restClientBuilder, Selectable.blank());
+		}
+
+	}
 
 }
