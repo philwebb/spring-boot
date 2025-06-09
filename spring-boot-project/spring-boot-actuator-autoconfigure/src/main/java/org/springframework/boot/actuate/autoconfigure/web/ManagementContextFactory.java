9e7bdca7e0839d5b00381867c17e0a6949b84c68
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-06-09 09:51:18 +0100

    Move some tests out of actuator-autoconfigure-all

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/web/ManagementContextFactory.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/web/ManagementContextFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -72,7 +72,9 @@ public final class ManagementContextFactory {
 
 	public void registerWebServerFactoryBeans(ApplicationContext parentContext,
 			ConfigurableApplicationContext managementContext, AnnotationConfigRegistry registry) {
-		registry.register(this.autoConfigurationClasses);
+		if (this.autoConfigurationClasses != null && this.autoConfigurationClasses.length > 0) {
+			registry.register(this.autoConfigurationClasses);
+		}
 		registerWebServerFactoryFromParent(parentContext, managementContext);
 	}
 
